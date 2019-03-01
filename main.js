c49b50debce243eabf3936f9270d0b4496d27fb5
Nigell Vyent <nvyent@air-van-nigell.home>
2019-03-01 12:21:57 +0100

    first commit

---
+++ main.js
@@ -0,0 +1,13 @@
+var massage1 = new Vue({
+    el: '#massage1',
+    data: {
+        massage1: 'Wat ik het liefs wil leren op deze stageplek is: zelfstandig mijn code kan schrijven. ik wil graag de meest gebruikte codes leren waar je mee werkt op de werkvloer, ik wil leren werken met frameworks en als het nodig is werken met python. Ik wil graag weten hoe het bedrijf leven is en waarom het zo leuk en interessant kan zijn om te werken bij een bedrijf zoals Brand new guys. Ik wil onderzoek of dit wel echt mijn ding in en of ik hier mijn brood mee wil verdienen. Na het beheersen van de basis van dit vakgebied moet ik al weten of ik daadwerkelijk wil doorstuderen in deze sector. En daar kan Brand new guys mij mee helpen. '
+    }
+});
+
+var massage2 = new Vue({
+    el: '#massage2',
+    data: {
+        massage2: ' het leukste wat ik vind aan webtechnollogie is dat er zoveel mogelijkheden zijn. Er wordt altijd iets nieuws uitgevonden waar je mee kan werken. Als je deze basis weet, weet je bij wijze van spreken nog geen 25% van wat je nog alleemaal nog meer kan doen. Je kan websites bouwen op een volledig eigen manier. Je kan je pagina zoveel dingen laten doen zoals: een bericht laten typen als iemand op je pagina zit zonder het zelf te typen, je kan een bot maken die allemaal vragen kan laten beantwoorden zonder zelf antwoord te geven etc. maar het programmeren van zoiets spreekt mij aan. Als je dit beheerst en goed kan, verdien je er ook gewoon veeeeel geld mee. '
+    }
+});
